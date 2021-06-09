3fd7cc0299f1451384f8ec0289b6527993d81a61
Russell Johnson <joh14233@umn.edu>
2021-06-09 09:02:41 -0700

    Adding task.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
 
 function App() {
-    const name = 'Russell'
-    const x = false
+    const [tasks, setTasks] = useState(  [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 1,
+            text: 'Haircut',
+            day: 'Feb 6th at 2:30pm',
+            reminder: true
+        },
+    ])
   return (
     <div className="container">
      <Header  />
+     <Tasks tasks={tasks} />
     </div>
   );
 }
